c8dce32db352ffdbd2f9747de823304c6caa5953
Hour Seth Thavareak <141904301+SethThavareakHour@users.noreply.github.com>
2024-12-14 19:20:42 +0700

    Update bookmarks.component.ts

--- src/app/bookmarks/bookmarks.component.ts
+++ src/app/bookmarks/bookmarks.component.ts
@@ -1,33 +1,3 @@
-// import { Component, OnDestroy, OnInit } from '@angular/core';
-// import { BookmarkTileComponent } from "../bookmark-tile/bookmark-tile.component";
-// import { BookmarkService } from '../shared/bookmark.service';
-// import { Bookmark } from '../shared/bookmark.model';
-// import { CommonModule } from '@angular/common';
-// import { RouterModule } from '@angular/router';
-
-// @Component({
-//   selector: 'app-bookmarks',
-//   standalone: true,
-//   imports: [BookmarkTileComponent, CommonModule, RouterModule],
-//   templateUrl: './bookmarks.component.html',
-//   styleUrls: ['./bookmarks.component.scss']
-// })
-// export class BookmarksComponent implements OnInit {
-
-//   bookmarks: Bookmark[]=[]
-
-//   constructor(private bookmarkService: BookmarkService ) {
-
-//   }
-  
-//   ngOnInit(): void {
-//     this.bookmarks = this.bookmarkService.getBookmarks()
-//   }
-
-// }
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { BookmarkTileComponent } from "../bookmark-tile/bookmark-tile.component";
 import { BookmarkService } from '../shared/bookmark.service';
